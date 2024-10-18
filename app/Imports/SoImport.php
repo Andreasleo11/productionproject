efb0e66f9885ebe3bf68f11b9584f21033dfde07
Andreas Leonardo Soenarto <Andreasleonardo.al@gmail.com>
2024-10-18 14:21:59 +0700

    import fix for so_datas

--- app/Imports/SoImport.php
+++ app/Imports/SoImport.php
@@ -9,11 +9,43 @@ class SoImport implements ToModel
 {
     public function model(array $row)
     {
+
+        $amendRecordhard = SoData::where('id', $row[0])
+                         ->where('doc_num', '!=', $row[1])
+                         ->first();
+
+        
+        // If the record exists, update it with the new data
+        if ($amendRecordhard) {
+            \Log::info('Found record with different doc_num and item_code. Updating...');
+
+            $amendRecordhard->update([
+                'doc_num'            => $row[1],
+                'customer'           => $row[2],
+                'posting_date'       => $row[3],
+                'item_code'          => $row[4],  // Update to new item_code from the Excel file
+                'item_name'          => $row[5],
+                'quantity'           => $row[6],
+                'sales_uom'          => $row[7],
+                'packaging_quantity' => $row[8],
+                'sales_pack'         => $row[9],
+                'create_date'        => $row[10],
+                'update_date'        => $row[11],
+                'update_fulltime'    => $row[12],
+                'is_finish'          => $amendRecordhard->is_finish,  // Keep is_finish unchanged
+                'is_done'            => $amendRecordhard->is_done,    // Keep is_done unchanged
+            ]);
+
+            return $amendRecordhard;  // Return the updated record
+        }
+
+
         $amendRecord = SoData::where('id', $row[0])
                          ->where('doc_num', $row[1])
                          ->where('item_code', '!=', $row[4])
                          ->first();
 
+      
         // If the record exists, update it with the new data
         if ($amendRecord) {
             \Log::info('Found record with matching doc_num but different item_code. Updating...');
@@ -42,7 +74,7 @@ class SoImport implements ToModel
         $existingRecord = SoData::where('doc_num', $row[1])
                                 ->where('item_code', $row[4])
                                 ->first();
-
+        
         // If record exists, skip it by returning null
         if ($existingRecord) {
             \Log::info('Duplicate found for doc_num: ' . $row[1] . ' and item_code: ' . $row[4]);
@@ -73,7 +105,7 @@ class SoImport implements ToModel
             return $existingRecord;
         }
 
-        
+        // dd($row);
 
         return new SoData([
             'id'                 => $row[0],
